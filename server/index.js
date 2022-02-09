a4a34497e62dbbd5d3179c2720c13e25c7472944
Pardhu Madipalli <pardhu.madipalli@oracle.com>
2022-02-09 22:15:24 +0530

    First time added client and server code

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3001
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+app.get('/api', (req, res) => {
+    res.json({ message: "Hello from server Pardhu!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
