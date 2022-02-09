54d1182a301bf78e40cf4b11d3292a499976e514
Pardhu Madipalli <pardhu.madipalli@oracle.com>
2022-02-09 22:23:12 +0530

    use environment variable PORT

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3001
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -15,6 +15,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
